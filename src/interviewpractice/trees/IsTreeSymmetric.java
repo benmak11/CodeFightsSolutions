f3c60f2e2a95e93366a38421a601dd3edcd77ff3
Ben Makusha <btmakusha@gmail.com>
2018-02-19 19:15:53 -0500

    isTreeSymmetric solution

---
+++ src/interviewpractice/trees/IsTreeSymmetric.java
@@ -0,0 +1,22 @@
+package interviewpractice.trees;
+
+/**
+ * @author benmakusha
+ */
+
+public class IsTreeSymmetric {
+
+    public boolean isTreeSymmetric(Tree<Integer> t) {
+        return isItAMirror(t, t);
+    }
+
+    public boolean isItAMirror(Tree t1, Tree t2) {
+        if (t1 == null && t2 == null)
+            return true;
+        if (t1 == null || t2 == null)
+            return false;
+        return (t1.value).equals(t2.value)
+                && isItAMirror(t1.left, t2.right)
+                && isItAMirror(t1.right, t2.left);
+    }
+}
