ebfd501ff26baf40b05e17b1a027e4a89ef3ce54
Ben Makusha <btmakusha@gmail.com>
2018-02-25 12:43:43 -0500

    arrayMaxConsecutiveSum2 solution

--- src/interviewpractice/commonTechniquesBasic/ArrayMaxConsecutiveSum2.java
+++ src/interviewpractice/commonTechniquesBasic/ArrayMaxConsecutiveSum2.java
@@ -4,4 +4,20 @@ package interviewpractice.commonTechniquesBasic;
  * @author benmakusha
  */
 public class ArrayMaxConsecutiveSum2 {
+
+    int arrayMaxConsecutiveSum2(int[] inputArray) {
+        int max_so_far = Integer.MIN_VALUE;
+        int max_ending_here = 0;
+
+        for (int i = 0; i < inputArray.length; i++) {
+            max_ending_here = max_ending_here + inputArray[i];
+            if (max_so_far < max_ending_here)
+                max_so_far = max_ending_here;
+            if (max_ending_here < 0)
+                max_ending_here = 0;
+        }
+
+        return max_so_far;
+    }
+
 }
