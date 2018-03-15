91c380cd277e405f6fceb9044e5dfcd2a6b58942
Ben Makusha <btmakusha@gmail.com>
2018-03-15 11:42:05 -0400

    climbingStairs solution

--- src/interviewpractice/dynamicprogrammingbasic/ClimbingStairs.java
+++ src/interviewpractice/dynamicprogrammingbasic/ClimbingStairs.java
@@ -4,4 +4,16 @@ package interviewpractice.dynamicprogrammingbasic;
  * @author benmakusha
  */
 public class ClimbingStairs {
+
+    int climbingStairs(int n) {
+        if (n <= 2) return n;
+
+        int[] numbers = new int[n];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        for (int i = 2; i < n; i++) {
+            numbers[i] = numbers[i - 1] + numbers[i - 2];
+        }
+        return numbers[n-1];
+    }
 }
